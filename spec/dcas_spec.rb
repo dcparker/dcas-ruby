eedceddc6e057750eda602ac75f6d6c36dcead8a
dcparker <dcparker@gmail.com>
2010-01-12 14:43:18 -0500

    Worked in some basic tests.

--- spec/dcas_spec.rb
+++ spec/dcas_spec.rb
@@ -1,14 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Dcas - Comprehensive failure frequency test" do
+  it "should generate Ach payment files correctly" do
+    ach_payments_file = DCAS.generate!(Fixtures[:TestPayments][:Ach].collect {|p| DCAS::AchPayment.new(p) })
+    ach_payments_file.should eql(File.read('spec/fixtures/ach_payments.csv'))
+  end
+
+  it "should generate CreditCard payment files correctly" do
+    credit_card_payments_file = DCAS.generate!(Fixtures[:TestPayments][:CreditCard].collect {|p| DCAS::CreditCardPayment.new(p) })
+    credit_card_payments_file.should eql(File.read('spec/fixtures/credit_card_payments.csv'))
+  end
+
   it "should be able to complete an entire mock procedure without failing" do
-    clients = [] # fixture load of a list of DCAS logins to test
-    files = [] # fixed test files
-    clients.each do |client|
-      client.submit_files!(files)
-    end
-    clients.each do |client|
-      client.download_response_files!
-    end
+    lambda {
+      # Depends: Fixture load of a list of DCAS logins to test
+      # Depends: Fixed test files
+      Fixtures[:Clients].each do |client|
+        client.submit_files!(Fixtures[:PaymentFiles])
+      end
+      Fixtures[:Clients].each do |client|
+        client.download_response_files!
+      end
+    }.should_not raise_error
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,12 @@ require 'spec/autorun'
 Spec::Runner.configure do |config|
   
 end
+
+Fixtures = {
+  :Clients => YAML.load_file('spec/fixtures/clients.yml').collect {|c| DCAS::Client.new(c) },
+  :TestPayments => {
+    :Ach => YAML.load_file('spec/fixtures/ach_payments.yml'),
+    :CreditCard => YAML.load_file('spec/fixtures/credit_card_payments.yml')
+  }
+  :PaymentFiles => Dir.glob("spec/fixtures/*.csv")
+}
